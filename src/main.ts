d2e1a39ae94c44335bc4fabe68fcb5199fa68495
BotFragger <m.ronikusuma@gmail.com>
2025-09-08 03:40:59 +0700

    create user

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Inventory Management')
+    .setDescription('Level 3 of NestJS learn')
+    .setVersion('1.0')
+    .addTag('InventoryManagement')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
---
+++ src/user/user.service.ts
@@ -0,0 +1,54 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+  async create(createUserDto: CreateUserDto) {
+    const existingUser = await this.prisma.user.findFirst({
+      where: {
+        OR: [{ name: createUserDto.name }, { email: createUserDto.email }],
+        deletedAt: null,
+      },
+    });
+
+    if (existingUser) {
+      if (existingUser.email === createUserDto.email) {
+        throw new ConflictException('Email sudah dipakai');
+      }
+    }
+
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const user = await this.prisma.user.create({
+      data: {
+        name: createUserDto.name,
+        email: createUserDto.email,
+        password: hashedPassword,
+      },
+    });
+
+    return {
+      data: user,
+      message: 'User berhasil dibuat',
+    };
+  }
+
+  async findAll() {
+    return `This action returns all user`;
+  }
+
+  async findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  async remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
